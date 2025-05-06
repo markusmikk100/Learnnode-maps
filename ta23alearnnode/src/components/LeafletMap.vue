43498643785bb215aa05d6a5c22a94c86bdf8732
markusmikk100 <TauriTammela272@gmail.com>
2025-05-06 19:14:50 +0300

    oma kodu kordinaatidega nupp go to home, marker kodu kohale ja polygon ümber.

--- ta23alearnnode/src/components/LeafletMap.vue
+++ ta23alearnnode/src/components/LeafletMap.vue
@@ -1,37 +1,55 @@
 <script setup>
 //import * as L from 'leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet/dist/images/marker-icon-2x.png';
-import 'leaflet/dist/images/marker-icon.png';
-import 'leaflet/dist/images/marker-shadow.png';
-import { onMounted, useId, watch } from 'vue';
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet/dist/images/marker-icon-2x.png";
+import "leaflet/dist/images/marker-icon.png";
+import "leaflet/dist/images/marker-shadow.png";
+import { onMounted, useId, watch } from "vue";
 
-const {center, zoom} = defineProps(['center', 'zoom']);
-const id = 'map-' + useId();
+const { center, zoom } = defineProps(["center", "zoom"]);
+const id = "map-" + useId();
 let map;
 onMounted(() => {
-    map = L.map(id).setView(center, zoom);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    var marker = L.marker([59.4269, 24.7434]).addTo(map);
+	map = L.map(id).setView(center, zoom);
+	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+		maxZoom: 19,
+		attribution:
+			'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	}).addTo(map);
+	var marker = L.marker([59.4269, 24.7434]).addTo(map);
+	var marker = L.marker([59.438511, 24.790931]).addTo(map);
+	var polygon = L.polygon([
+		[59.438690065086675, 24.790947325689241],
+		[59.43866707980278, 24.791067223474258],
+		[59.438715068286356, 24.791088265393288],
+		[59.438679, 24.791309],
+		[59.43827796911317, 24.7910285862434],
+		[59.438314905473604, 24.790815191005905],
+		[59.438354115024964, 24.790848048064333],
+		[59.438374799682464, 24.790736225470305],
+	]).addTo(map);
 });
-watch(() => center, (center, oldCenter) => {
-    console.log(center, oldCenter);
-    map.panTo(center);
-});
+watch(
+	() => center,
+	(center, oldCenter) => {
+		console.log(center, oldCenter);
+		map.panTo(center);
+	}
+);
 
-watch(() => zoom, (zoom) => {
-    map.setZoom(zoom);
-});
+watch(
+	() => zoom,
+	(zoom) => {
+		map.setZoom(zoom);
+	}
+);
 </script>
 <template>
-     <div :id="id"></div>
+	<div :id="id"></div>
 </template>
 <style scoped>
-div { 
-    height: 90vh;
+div {
+	height: 90vh;
 }
 </style>
--- ta23alearnnode/src/pages/Leaflet.vue
+++ ta23alearnnode/src/pages/Leaflet.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { ref } from 'vue';
-import LeafletMap from '../components/LeafletMap.vue';
+import { ref } from "vue";
+import LeafletMap from "../components/LeafletMap.vue";
 
 let center = ref([59.4269, 24.7434]);
 let zoom = ref(19);
 </script>
 <template>
-        <button class="button is-primary" @click="center=[59.41071145958258, 24.60537846324927]">Go To Home</button>
-     <input type="range" min="1" max="19" v-model="zoom">
-        <LeafletMap :center="center" :zoom="zoom"></LeafletMap>
-     <LeafletMap :center="[24.4269, 59.7434]" :zoom="19"></LeafletMap>
+	<button class="button is-primary" @click="center = [59.438511, 24.790931]">
+		Go To Home
+	</button>
+	<input type="range" min="1" max="19" v-model="zoom" />
+	<LeafletMap :center="center" :zoom="zoom"></LeafletMap>
+	<LeafletMap :center="[24.4269, 59.7434]" :zoom="19"></LeafletMap>
 </template>
